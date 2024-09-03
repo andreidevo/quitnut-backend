7cff502d870ece141891baf1f9150ceb01292630
Andrei Novatov <apppannnappp@gmail.com>
2024-09-03 10:32:55 +0200

    awdawd

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -88,6 +88,7 @@ function appendNewToken(req, res, next) {
 
         res.json = function (data) {
           console.log("wtf");
+          console.log(typeof data);
           console.log(data);
           if (res.newAccessToken && typeof data === 'object' && data !== null) {
               data.newAccessToken = res.newAccessToken;  // Append new token
