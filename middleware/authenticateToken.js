3bee0bfe7b83d714a25af1a342a6a3c2ca7d06f8
Andrei Novatov <apppannnappp@gmail.com>
2024-09-03 18:02:22 +0200

    awdawd

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -50,32 +50,59 @@ async function verifyJWT(req, res, next) {
   
     next();
   } catch (err) {
+    console.log("MMM2");
+
+    if (err.name === 'TokenExpiredError') {
+      console.log("MMM3");
+
+      const decoded = jwt.decode(token);
+      console.log(decoded);
+      
+      if (decoded && decoded._id) {
+          // Now attempt to refresh the token using the user's ID
+          const result = await refreshUserTokens(decoded._id);
+          if (result.status === 200) {
+              // Set the new token in the request header and user object
+              req.headers.authorization = `Bearer ${result.accessToken}`;
+              req.user = jwt.verify(result.accessToken, process.env.JWT_SECRET);
+              req.newAccessToken = result.accessToken; // Optionally pass new token back in response
+              next();
+          } else {
+              res.status(result.status).json({ message: result.error });
+          }
+      } else {
+          res.status(403).send('Invalid token, unable to refresh');
+      }
+    } else {
+      // For other errors, send a generic 403 response
+      res.status(403).send('Invalid token');
+    }
     console.log(err);
     console.log("MMM2");
 
-    console.log(req.user);
-    console.log(req.user._id);
+    // console.log(req.user);
+    // console.log(req.user._id);
 
-    if (err.name === 'TokenExpiredError' && req.user && req.user._id) {
-      console.log(err.name);
+    // if (err.name === 'TokenExpiredError' && req.user && req.user._id) {
+    //   console.log(err.name);
 
-      const result = await refreshUserTokens(req.user._id);
-      console.log(result);
-      if (result.status === 200) {
-          req.headers.authorization = `Bearer ${result.accessToken}`;  // Optionally set new access token in headers
-          req.user = jwt.verify(result.accessToken, process.env.JWT_SECRET);
+    //   const result = await refreshUserTokens(req.user._id);
+    //   console.log(result);
+    //   if (result.status === 200) {
+    //       req.headers.authorization = `Bearer ${result.accessToken}`;  // Optionally set new access token in headers
+    //       req.user = jwt.verify(result.accessToken, process.env.JWT_SECRET);
     
-          console.log("DONE");
-          next();
-      } else {
-          console.log("NOPE");
+    //       console.log("DONE");
+    //       next();
+    //   } else {
+    //       console.log("NOPE");
 
-          res.status(result.status).json({ message: result.error });
-      }
-    } else {
-        console.log("NOPE2");
-        res.sendStatus(403);
-    }
+    //       res.status(result.status).json({ message: result.error });
+    //   }
+    // } else {
+    //     console.log("NOPE2");
+    //     res.sendStatus(403);
+    // }
   }
 }
 
